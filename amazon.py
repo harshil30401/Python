9c2ad557cc136529d582d763c57e51877a71a134
Harshil Patel <67827200+harshil30401@users.noreply.github.com>
2021-07-26 00:46:22 +0530

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -2,6 +2,7 @@ from KITT import send_email
 import requests
 from bs4 import BeautifulSoup
 import time
+import smtplib as smtp
 
 URL = 'https://www.amazon.in/Beats-Powerbeats-Pro-Wireless-Earphones/dp/B07RB7YHWH/ref=sr_1_1_sspa?crid=11K03X88325K3&dchild=1&keywords=powerbeats+pro&qid=1626865138&sprefix=powerbeats+pr%2Caps%2C270&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFVVzNNN1NMRE1RNEYmZW5jcnlwdGVkSWQ9QTAyNjMyMDUzTE1NVVc5NEZWTTZGJmVuY3J5cHRlZEFkSWQ9QTA2NTQyMTExMkdJM1M1OUoyTVpHJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'}
@@ -19,15 +20,34 @@ def check_price():
 
     print(converted_price, title.strip())
 
+    receiver = '***************'
     subject = "Price fell down!"
-    body = "Check the link https://www.amazon.in/Beats-Powerbeats-Pro-Wireless-Earphones/dp/B07RB7YHWH/ref=sr_1_1_sspa?crid=11K03X88325K3&dchild=1&keywords=powerbeats+pro&qid=1626865138&sprefix=powerbeats+pr%2Caps%2C270&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFVVzNNN1NMRE1RNEYmZW5jcnlwdGVkSWQ9QTAyNjMyMDUzTE1NVVc5NEZWTTZGJmVuY3J5cHRlZEFkSWQ9QTA2NTQyMTExMkdJM1M1OUoyTVpHJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
-
-    msg = f"Subject: {subject} \n\n {body}"
-
+    body = URL
     if(converted_price < 15000):
-        send_email('harshil.patel@sakec.ac.in', msg, body)
+        send_email(receiver, subject, body)
 
     # print("Email sent!")
+
+def send_email(receiver, subject, body):
+    server = smtp.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    username = '***************'
+    password = '********'
+
+    server.login(username, password)
+    subject = subject
+    body = body
+    composed_message = f"Subject: {subject}\n\n {body}"
+    server.sendmail(
+        from_addr=username,
+        to_addrs= receiver,
+        msg=composed_message
+    )
+    print("EMAIL SENT!")
+    server.quit()
 
 while(True):
     check_price()
